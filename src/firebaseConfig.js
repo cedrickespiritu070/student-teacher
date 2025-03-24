9d342117befeae5c391eab686bec4e19d48abf92
darkrobin006 <jannus.viray@gmail.com>
2025-03-24 16:53:57 +0800

    updated firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaaWXiLmDAUbSDUb-ThvD-_IyU7Mg6m4M", // Replace with your actual API key
-  authDomain: "my-application--kainos.firebaseapp.com", // Replace with your actual auth domain
-  databaseURL: "https://my-application--kainos-default-rtdb.firebaseio.com", // Your database URL
-  projectId: "my-application--kainos", // Replace with your actual project ID
-  storageBucket: "my-application--kainos.appspot.com", // Your storage bucket URL
-  messagingSenderId: "1004543669813", // Your sender ID
-  appId: "1:1004543669813:android:34c9dc9e92a48638f2e37c",
+  apiKey: "AIzaSyCO8DJ0LRnyYzPhozvt-YrynqTBrQgyhCI", // Replace with your actual API key
+  authDomain: "commonsense-9c0d1.firebaseapp.com", // Replace with your actual auth domain
+  databaseURL: "https://commonsense-9c0d1-default-rtdb.firebaseio.com", // Your database URL
+  projectId: "commonsense-9c0d1", // Replace with your actual project ID
+  storageBucket: "commonsense-9c0d1.appspot.com", // Your storage bucket URL
+  messagingSenderId: "585230915074", // Your sender ID
+  appId: "1:585230915074:android:d1075032d8592b06b1f91b",
 };
 
 const app = initializeApp(firebaseConfig);
